38e15a42c35530a6b8a32948765f2682b470386b
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2025-04-26 23:47:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import logging
 from dotenv import load_dotenv
 
-# Configure Logging
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
